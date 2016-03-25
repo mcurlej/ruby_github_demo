7688c1a112fe3a2906821a26a0b4d86cf54f61b3
mcurlej <martin.curlej@gmail.com>
2016-03-25 16:24:51 +0100

    initial commit, created first working version

--- github.rb
+++ github.rb
@@ -11,6 +11,7 @@ class GithubAPI
   @@apiUrlsTemplates = {
     "issuesOpenUrl" => "https://api.github.com/search/issues?q=repo:user/repository+is:issue+is:open+&per_page=1",
     "issuesClosedUrl" => "https://api.github.com/search/issues?q=repo:user/repository+is:issue+is:closed+&per_page=1",
+
     #"repositoryUrl" => "https://api.github.com/repos/user/repository"
   }
 
@@ -42,7 +43,7 @@ class GithubAPI
 
     configuredUrls.push(URI(@@apiUrlsTemplates["issuesOpenUrl"].sub(/(.+\?q=repo:)user\/repository(.+)/, "\\1#{@user}\/#{@repository}\\2")))
     configuredUrls.push(URI(@@apiUrlsTemplates["issuesClosedUrl"].sub(/(.+\?q=repo:)user\/repository(.+)/, "\\1#{@user}\/#{@repository}\\2")))
-    #configuredUrls.push(URI(@@apiUrlsTemplates["repositoryUrl"].sub(/(.+?\/repos\/)user\/repository/, "\\1#{@user}\/#{@repository}")))
+    configuredUrls.push(URI(@@apiUrlsTemplates["repositoryUrl"].sub(/(.+?\/repos\/)user\/repository/, "\\1#{@user}\/#{@repository}")))
     return configuredUrls
   end
 end
@@ -70,9 +71,11 @@ class GithubRepository < GithubAPI
   end
 
   def formatData(repositoryData)
-    #puts repositoryData[2]["full_name"]
-    puts repositoryData[0]["total_count"]
-    puts repositoryData[1]["total_count"]
+    openIssues = repositoryData[0]["total_count"]
+    closedIssues = repositoryData[1]["total_count"]
+    repoName = repositoryData[2]["full_name"]
+    puts repoName
+    puts "Issues (#{openIssues+closedIssues})"
 
     #return repositoryData
   end
